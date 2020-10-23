2ecb8cdae7d19403edc984644ffeab29883bff26
Brian Gu <briangu33@gmail.com>
2020-10-22 21:50:20 -0400

    zoop

--- client/src/app/board/AbstractUIManager.ts
+++ client/src/app/board/AbstractUIManager.ts
@@ -1,7 +1,10 @@
 import {EventEmitter} from 'events';
+import {BoardLocation, ChessGame} from '../../_types/global/GlobalTypes';
 
 export default interface AbstractUIManager extends EventEmitter {
   destroy(): void;
 
   joinGame(): Promise<void>;
+  getGameState(): ChessGame;
+  movePiece(pieceId: number, to: BoardLocation): void;
 }
