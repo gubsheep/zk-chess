2b83c6e2d973cfdab1bb23261148a76341fc07ff
Alan Luo <me@alanluo.com>
2020-10-22 21:39:51 -0400

    Update LandingPage.tsx

--- client/src/app/LandingPage.tsx
+++ client/src/app/LandingPage.tsx
@@ -45,7 +45,6 @@ export function LandingPage() {
   const displayAccounts = () => {
     const ethConnection = EthereumAccountManager.getInstance();
     const knownAccounts = ethConnection.getKnownAccounts();
-    if ()
     ethConnection.addAccount(
       '0x044C7963E9A89D4F8B64AB23E02E97B2E00DD57FCB60F316AC69B77135003AEF'
     );
