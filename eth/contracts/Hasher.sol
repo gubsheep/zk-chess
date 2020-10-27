2c7b662840211dc6be341e9a9a6b412bd4b5d4b5
Brian Gu <briangu33@gmail.com>
2020-10-27 16:38:50 -0400

    add some comments

--- eth/contracts/Hasher.sol
+++ eth/contracts/Hasher.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.6.7;
 pragma experimental ABIEncoderV2;
 
 library Hasher {
+    // this library is never actually deployed. rather, it serves as a
+    // placeholder or "abstract library" so that contracts who depend on
+    // https://github.com/iden3/circomlib/blob/master/src/mimcsponge_gencontract.js
+    // can compile. the above bytecode should be deployed and linked to dependant
+    // think of this like a poor man's header file
     function MiMCSponge(
         uint256 in_xL,
         uint256 in_xR,
